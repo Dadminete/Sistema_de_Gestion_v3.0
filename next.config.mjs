8cc6f7933007af093643e89981b1ba0a883ecf03
Daniel Beras <empresatecnologicadeleste@gmail.com>
2026-02-07 06:17:29 -0400

    chore: add lan dev script and crm redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,14 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
+  allowedDevOrigins: ["http://172.16.0.23:3000"],
   async redirects() {
     return [
+      {
+        source: "/",
+        destination: "/dashboard/crm",
+        permanent: false,
+      },
       {
         source: "/dashboard",
         destination: "/dashboard/default",
